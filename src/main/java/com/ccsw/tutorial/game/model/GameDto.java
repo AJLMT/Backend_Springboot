0bae1237a9f3c8b9504aedad671757f8283fef75
AJLMT <ajlmthosab30@yahoo.es>
2025-02-19 12:02:15 +0100

    Backend de Bootstrap

---
+++ src/main/java/com/ccsw/tutorial/game/model/GameDto.java
@@ -0,0 +1,118 @@
+package com.ccsw.tutorial.game.model;
+
+import com.ccsw.tutorial.author.model.AuthorDto;
+import com.ccsw.tutorial.category.model.CategoryDto;
+
+/**
+ * @author ccsw
+ *
+ */
+public class GameDto {
+
+    private Long id;
+
+    private String title;
+
+    private String age;
+
+    private CategoryDto category;
+
+    private AuthorDto author;
+
+    private String img;
+
+    /**
+     * @return id
+     */
+    public Long getId() {
+
+        return this.id;
+    }
+
+    /**
+     * @param id new value of {@link #getId}.
+     */
+    public void setId(Long id) {
+
+        this.id = id;
+    }
+
+    /**
+     * @return title
+     */
+    public String getTitle() {
+
+        return this.title;
+    }
+
+    /**
+     * @param title new value of {@link #getTitle}.
+     */
+    public void setTitle(String title) {
+
+        this.title = title;
+    }
+
+    /**
+     * @return age
+     */
+    public String getAge() {
+
+        return this.age;
+    }
+
+    /**
+     * @param age new value of {@link #getAge}.
+     */
+    public void setAge(String age) {
+
+        this.age = age;
+    }
+
+    /**
+     * @return category
+     */
+    public CategoryDto getCategory() {
+
+        return this.category;
+    }
+
+    /**
+     * @param category new value of {@link #getCategory}.
+     */
+    public void setCategory(CategoryDto category) {
+
+        this.category = category;
+    }
+
+    /**
+     * @return author
+     */
+    public AuthorDto getAuthor() {
+
+        return this.author;
+    }
+
+    /**
+     * @param author new value of {@link #getAuthor}.
+     */
+    public void setAuthor(AuthorDto author) {
+
+        this.author = author;
+    }
+
+    /**
+     * @return img
+     */
+    public String getImg() {
+        return this.img;
+    }
+
+    /**
+     * @param img new value of {@link #getImg}.
+     */
+    public void setImg(String img) {
+        this.img = img;
+    }
+
+}
---
+++ src/main/resources/data.sql
@@ -0,0 +1,17 @@
+INSERT INTO category(name) VALUES ('Eurogames');
+INSERT INTO category(name) VALUES ('Ameritrash');
+INSERT INTO category(name) VALUES ('Familiar');
+
+INSERT INTO author(name, nationality) VALUES ('Alan R. Moon', 'US');
+INSERT INTO author(name, nationality) VALUES ('Vital Lacerda', 'PT');
+INSERT INTO author(name, nationality) VALUES ('Simone Luciani', 'IT');
+INSERT INTO author(name, nationality) VALUES ('Perepau Llistosella', 'ES');
+INSERT INTO author(name, nationality) VALUES ('Michael Kiesling', 'DE');
+INSERT INTO author(name, nationality) VALUES ('Phil Walker-Harding', 'US');
+
+INSERT INTO game(title, age, category_id, author_id, img) VALUES ('On Mars', '14', 1, 2, 'On_Mars.png');
+INSERT INTO game(title, age, category_id, author_id, img) VALUES ('Aventureros al tren', '8', 3, 1, 'Aventureros_al_Tren.png');
+INSERT INTO game(title, age, category_id, author_id, img) VALUES ('1920: Wall Street', '12', 1, 4, 'wall_Street.png');
+INSERT INTO game(title, age, category_id, author_id, img) VALUES ('Barrage', '14', 1, 3, 'Barrage.png');
+INSERT INTO game(title, age, category_id, author_id, img) VALUES ('Los viajes de Marco Polo', '12', 1, 3, 'Marco_Polo.png');
+INSERT INTO game(title, age, category_id, author_id, img) VALUES ('Azul', '8', 3, 5, 'Azul.png');
